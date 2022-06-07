9370b86a3b91846d46e51bde216e6113b309a639
Camilla <camilla.taina.santos@accenture.com>
2022-06-07 12:25:33 -0300

    servidor heroku

--- server.js
+++ server.js
@@ -12,30 +12,4 @@ app.get('/*', (req, res) => {
 
 app.listen(PORT, () => {
   console.log('servidor iniciado na porta ' + PORT)
-})
-
-// const express = require('express');
-// const path = require('path');
-// const nomeApp = process.env.npm_package_name;
-// const app = express();
-
-// app.use(express.static(`${__dirname}/dist/${nomeApp}`));
-
-// app.get('/*', (req, res) => {
-// res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-// });
-
-// app.listen(process.env.PORT || 6200);
-
-
-// const express = require('express');
-
-// const app = express();
-// app.use(express.static('./dist/web-liferay'));
-
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/web-liferay/'}),
-// );
-
-
-// app.listen(process.env.PORT || 5000);
+});
